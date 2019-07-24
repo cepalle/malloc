21e7036c4dad97970d9e7ee0b5bc52f4a429cd89
epalle <epalle.cyrille@gmail.com>
2019-07-24 22:00:17 +0200

    sav

--- src/realloc.c
+++ src/realloc.c
@@ -12,16 +12,16 @@ void *realloc(void *ptr, size_t size) {
 
 	if (hd->size >= size) return ptr;
 
-	if (hd->next != NULL && hd->next->is_free) {
+	if (hd->enum_page_size != ENUM_PAGE_SIZE_LARGE && hd->next != NULL && hd->next->is_free) {
 		hd->size = hd->size + hd->next->size + sizeof(t_header);
 		hd->next = hd->next->next;
+
+		if (hd->size >= size) return ptr;
 	}
 
-	if (hd->size >= size) return ptr;
-
-	void *new_mem = malloc(size);
+	t_header *new_mem = malloc(size);
 	if (new_mem == NULL) return NULL;
 
-	ft_memmove(new_mem, hd + 1, ft_min(hd->size, size));
+	ft_memmove(new_mem, hd + 1, ft_min(hd->size, (new_mem - 1)->size));
 	return new_mem;
 }
