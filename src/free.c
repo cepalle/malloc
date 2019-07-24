53577e250920d85c46672644714c6c4611fe76dc
epalle <epalle.cyrille@gmail.com>
2019-07-24 16:50:37 +0200

    up large

--- src/free.c
+++ src/free.c
@@ -1,10 +1,31 @@
 #include "malloc.h"
 
 
-static void ft_free_in_header(void *ptr, t_header *h) {
+static void ft_free(t_header *ptr, t_header **h) {
 	if (h == NULL || ptr == NULL) return;
 
 
+}
+
+// --- LARGE
+
+static void ft_free_large(t_header *pf, t_header **h) {
+	if (*h == NULL) return;
+
+	t_header *it = *h;
+	while (it != NULL) {
+		if (it == pf) {
+
+			if (it->prev != NULL) {
+				it->prev->next = it->next;
+			} else {
+				*h = it->next;
+			}
+
+			munmap(pf, pf->size + sizeof(t_header));
+			return;
+		}
+	}
 }
 
 void free(void *ptr) {
@@ -14,11 +35,19 @@ void free(void *ptr) {
 	pi--;
 
 	if (pi->enum_page_size == ENUM_PAGE_SIZE_LARGE) {
-		// TODO
-		munmap(pi, pi->size + sizeof(t_header));
+		ft_free_large(
+				pi,
+				&(g_state.large)
+		);
 	} else if (pi->enum_page_size == ENUM_PAGE_SIZE_SMALL) {
-		ft_free_in_header(ptr, g_state.small);
+		ft_free(
+				pi,
+				&(g_state.small)
+		);
 	} else {
-		ft_free_in_header(ptr, g_state.tiny);
+		ft_free(
+				pi,
+				&(g_state.tiny)
+		);
 	}
 }
--- src/malloc.c
+++ src/malloc.c
@@ -5,13 +5,15 @@ static void *ft_malloc_data(size_t size, t_header_data *d) {
 
 }
 
-static void *ft_malloc(size_t size, t_header *h) {
+static void *ft_malloc(size_t size, t_header **h) {
 	t_header *d = h + 1;
 
 
 }
 
-static void *ft_malloc_large(size_t size) {
+// --- LARGE
+
+static void *ft_malloc_large_page(size_t size) {
 	size_t align = ft_align_size(size + sizeof(t_header), 4096);
 	t_header *new_mem = ft_mmap(align);
 	if (new_mem == NULL) return NULL;
@@ -26,12 +28,38 @@ static void *ft_malloc_large(size_t size) {
 	return new_mem;
 }
 
+static void *ft_malloc_large(size_t size, t_header **h) {
+	t_header *new_page = ft_malloc_large_page(size);
+	if (new_page == NULL) return NULL;
+
+	if (*h == NULL) {
+		*h = new_page;
+	} else {
+		t_header *it = *h;
+		while (it->next) {
+			it = it->next;
+		}
+		it->next = new_page;
+	}
+
+	return new_page + 1;
+}
+
 void *malloc(size_t size) {
 	if (size <= PAGE_TINY_DATA_SIZE) {
-		return ft_malloc(ft_align_size(size, PAGE_TINY_RES), g_state.tiny);
+		return ft_malloc(
+				ft_align_size(size, PAGE_TINY_RES),
+				&(g_state.tiny)
+		);
 	} else if (size <= PAGE_SMALL_DATA_SIZE) {
-		return ft_malloc(ft_align_size(size, PAGE_SMALL_RES), g_state.small);
+		return ft_malloc(
+				ft_align_size(size, PAGE_SMALL_RES),
+				&(g_state.small)
+		);
 	} else {
-		return ft_malloc_large(size);
+		return ft_malloc_large(
+				size,
+				&(g_state.large)
+		);
 	}
 }
